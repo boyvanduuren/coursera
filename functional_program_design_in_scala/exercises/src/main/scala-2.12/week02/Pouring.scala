8fb9146e2f0c9d7ddd52259bcbf44bd0e8540d20
Boy van Duuren <boy.van.duuren@raaftech.com>
2017-05-31 10:22:23 +0200

    More work on Pouring

--- functional_program_design_in_scala/exercises/src/main/scala-2.12/week02/Pouring.scala
+++ functional_program_design_in_scala/exercises/src/main/scala-2.12/week02/Pouring.scala
@@ -7,10 +7,22 @@ case class Pouring(capacity: Vector[Int]) {
   val initialState = capacity map (_ => 0)
 
   // Moves
-  trait Move
-  case class Empty(glass: Int) extends Move
-  case class Fill(glass: Int) extends Move
-  case class Pour(from: Int, to: Int) extends Move
+  trait Move {
+    def change(state: State): State
+  }
+
+  case class Empty(glass: Int) extends Move {
+    override def change(state: State): State = state updated (glass, 0)
+  }
+  case class Fill(glass: Int) extends Move {
+    override def change(state: State): State = state updated (glass, capacity(glass))
+  }
+  case class Pour(from: Int, to: Int) extends Move {
+    override def change(state: State): State = {
+      val amount = state(from) min (capacity(to) - state(to))
+      state updated (from, state(from) - amount) updated (to, state(to) + amount)
+    }
+  }
 
   val glasses = capacity.indices
 
@@ -18,4 +30,15 @@ case class Pouring(capacity: Vector[Int]) {
     (for (g <- glasses) yield Empty(g)) ++
     (for (g <- glasses) yield Fill(g)) ++
     (for (from <- glasses; to <- glasses if from != to) yield Pour(from, to))
+
+  // Paths
+  class Path(history: List[Move]) {
+    def endState: State = (history foldRight initialState) (_ change _)
+    def extend(move: Move) = new Path(move :: history)
+    override def toString = history.reverse mkString " " + "--> " + endState
+  }
+
+  val initialPath = new Path(Nil)
+
+  def from(paths: Set[Path]): Stream[Set[Path]] = ???
 }
