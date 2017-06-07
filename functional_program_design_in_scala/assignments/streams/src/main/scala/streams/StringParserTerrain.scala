1c1cbf4e1c6f33e56443fb0be8f0ad84180b34ca
B.C. van Duuren <boy@raaftech.com>
2017-06-07 15:09:24 +0200

    Implemented two functions in StringParserTerrain

--- functional_program_design_in_scala/assignments/streams/src/main/scala/streams/StringParserTerrain.scala
+++ functional_program_design_in_scala/assignments/streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,12 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean =
+    (pos: Pos) => {
+      if (pos.row < 0 || pos.col < 0 ||
+        pos.row > levelVector.size-1 || pos.col > levelVector.head.size-1) false
+      else (levelVector(pos.row))(pos.col) != '-'
+    }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +67,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos =
+  (levelVector filter (row => row contains c)).headOption match {
+    case Some(foundRow) => Pos(levelVector indexOf foundRow, foundRow indexOf c)
+    case None => Pos(-1, -1)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
