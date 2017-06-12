209a022e8b00a33f1196e584b6220c4b729ce90b
Boy van Duuren <boy.van.duuren@raaftech.com>
2017-06-12 17:24:38 +0200

    not working

--- functional_program_design_in_scala/assignments/streams/src/main/scala/streams/Solver.scala
+++ functional_program_design_in_scala/assignments/streams/src/main/scala/streams/Solver.scala
@@ -67,18 +67,24 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = initial match {
+    case Stream.Empty => Stream.empty
+    case (block, moves) #:: tail =>
+      val newNeighbors = newNeighborsOnly(neighborsWithHistory(block, moves), explored + block)
+      newNeighbors ++ from(newNeighbors ++ initial, explored + block)
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(Stream((startBlock, List())), Set(startBlock))
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] =
+  pathsFromStart filter { case (block, moves) => done(block) }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -88,5 +94,8 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal.headOption match {
+    case None => List()
+    case Some((block, moves)) => moves
+  }
 }
--- functional_program_design_in_scala/exercises/src/main/scala-2.12/week02/Scratch.sc
+++ functional_program_design_in_scala/exercises/src/main/scala-2.12/week02/Scratch.sc
@@ -3,4 +3,6 @@ def from(n: Int): Stream[Int] = n #:: from(n+1)
 def sieve(s: Stream[Int]): Stream[Int] =
   s.head #:: sieve(s.tail filter (_ % s.head != 0))
 
-(sieve(from(2)) take 10).toList
+(sieve(from(2)) take 100).toList
+
+from(1) map (x => x%2 == 0)
