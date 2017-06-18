22a5f336ee5f4cda6a4258547d2b1feb86cf0931
B.C. van Duuren <boy@vanduuren.xyz>
2017-06-18 14:02:24 +0200

    Finished assignment

--- functional_program_design_in_scala/assignments/calculator/src/main/scala/calculator/Polynomial.scala
+++ functional_program_design_in_scala/assignments/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,26 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal{
+      val aNow = a()
+      val bNow = b()
+      val cNow = c()
+      (bNow*bNow) - 4*(aNow*cNow)
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal{
+      val minB = b() * -1
+      val deltaNow = delta()
+      val doubleA = 2*a()
+      if (deltaNow < 0) Set()
+      else
+      Set(
+        (minB + Math.sqrt(deltaNow)) / doubleA,
+        (minB - Math.sqrt(deltaNow)) / doubleA
+      )
+    }
   }
 }
